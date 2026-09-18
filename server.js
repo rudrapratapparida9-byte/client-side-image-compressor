177d7359d51360758b07dcf1a6c5a5753af997b4
Rudra Pratap Parida <rudrapratapparida@users.noreply.github.com>
2026-09-18 19:04:20 +0530

    Fix XML MIME types in server.js

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 const PORT = 8080;
 const MIME_TYPES = {
-  '.html': 'text/html',
-  '.css': 'text/css',
-  '.js': 'text/javascript',
-  '.json': 'application/json',
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'text/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.xml': 'application/xml; charset=utf-8',
+  '.txt': 'text/plain; charset=utf-8',
   '.svg': 'image/svg+xml',
   '.png': 'image/png',
   '.jpg': 'image/jpeg',
